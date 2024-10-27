8934bbe281f6d3e6fa84e3010655da3d86cab0e2
ph2xc8 <109532432+Ph8042@users.noreply.github.com>
2024-10-27 11:16:28 +0100

    Main Window Callback,
    Alongside with Registering WindowClass and small refactoring

--- code/win32_window.cpp
+++ code/win32_window.cpp
@@ -1,20 +1,68 @@
 #include <windows.h>
 
+LRESULT CALLBACK
+MainWindowCallback(HWND Window, 
+                   UINT Message,
+                   WPARAM WParam,
+                   LPARAM LParam)
+{
+    LRESULT Result = 0;
+    
+    switch (Message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+        
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+        
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n");
+        } break;
+        
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+        
+        default:
+        {
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        } break;
+    }
+    
+    return (Result);
+}
+
 int CALLBACK
-WinMain(
-        HINSTANCE hInstance,
-        HINSTANCE hPrevInstance,
-        LPSTR lpCmdLine,
-        int nShowCmd
-        )
+WinMain(HINSTANCE Instance,
+        HINSTANCE PrevInstance,
+        LPSTR CommandLine,
+        int ShowCode)
 {
     WNDCLASS WindowClass = {};
     WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
     WindowClass.lpfnWndProc = ;
-    WindowClass.hInstance = hInstance;
+    WindowClass.hInstance = Instance;
     // WindowClass.hCursor;
     // WindowClass.hIcon; 
     WindowClass.lpszClassName = "Win32WindowClass";
+    
+    if (RegisterClassA(&WindowClass))
+    {
+        // Window Class Registration successful, the rest of our WinMain
+        // the rest of our WinMain
+    }
+    else
+    {
+        // Window Class Registration failed
+        // TODO: Logging
+    }
     
     return (0);
 }
