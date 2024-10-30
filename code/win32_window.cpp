87157093fefdd12c3a86dc3c00b5dea052cab8c5
ph2xc8 <109532432+Ph8042@users.noreply.github.com>
2024-10-30 23:53:16 +0100

    Finish Win32UpdateWindow
    Alongside with StretchDIBits, CreateDIBSection implementation.

--- code/win32_window.cpp
+++ code/win32_window.cpp
@@ -1,16 +1,53 @@
 #include <windows.h>
+#define internal static 
+#define local_persist static 
+#define global_variable static
+
 global_variable bool Running;
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
 
 internal void
 Win32ResizeDIBSection(int Width, int Height)
 {
+    BITMAPINFO BitmapInfo = {};
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
     
+    void *BitmapMemory;
+    
+    if (BitmapHandle)
+    {
+        DeleteObject(BitmapHandle);
+    }
+    
+    if(!BitmapDeviceContext)
+    {
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+    
+    BitmapHandle = CreateDIBSection(BitmapDeviceContext,
+                                    &BitmapInfo,
+                                    DIB_RGB_COLORS,       
+                                    &BitmapMemory, 
+                                    0, 0);
 }
 
 internal void
 Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
 {
-    
+    StretchDIBits(DeviceContext, 
+                  X, Y, Width, Height,
+                  X, Y, Width, Height,
+                  BitmapMemory,
+                  &BitmapInfo,
+                  DIB_RGB_COLORS, SRCCOPY);  
 }
 
 LRESULT CALLBACK
