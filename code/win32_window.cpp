39fc03438ba5b98d5c9e6d7dc19156c7437ffc36
ph2xc8 <109532432+Ph8042@users.noreply.github.com>
2024-11-11 00:54:51 +0100

    Extract ClientRect calculations

--- code/win32_window.cpp
+++ code/win32_window.cpp
@@ -1,8 +1,8 @@
 #include <windows.h>
 #include <stdint.h>
 
-#define internal static 
-#define local_persist static 
+#define internal static
+#define local_persist static
 #define global_variable static
 
 typedef uint8_t u8;
@@ -28,8 +28,27 @@ struct win32_offscreen_buffer
     int BytesPerPixel;
 };
 
+struct win32_window_dimension
+{
+    int Width;
+    int Height;
+};
+
 global_variable bool Running;
 global_variable win32_offscreen_buffer GlobalBackbuffer;
+
+internal win32_window_dimension
+Win32GetWindowDimension(HWND Window)
+{
+    win32_window_dimension Result;
+
+    RECT ClientRect;
+    GetClientRect(Window, &ClientRect);
+    Result.Width = ClientRect.right - ClientRect.left;
+    Result.Height = ClientRect.bottom - ClientRect.top;
+
+    return(Result);
+}
 
 internal void
 RenderGradient(win32_offscreen_buffer *Buffer, int XOffset, int YOffset)
@@ -47,7 +66,7 @@ RenderGradient(win32_offscreen_buffer *Buffer, int XOffset, int YOffset)
             // Pixel in memory: BB GG RR XX -> little endian
             u8 Red = X - Y;
             u8 Green = (u8)(X - YOffset);
-            u8 Blue = (u8)(X + XOffset);    
+            u8 Blue = (u8)(X + XOffset);
             *Pixel++ = Red << 16 | Green << 8 | Blue; // << 0
         }
         Row += Buffer->Pitch;
@@ -63,12 +82,12 @@ Win32ResizeDIBSection(win32_offscreen_buffer *Buffer, int Width, int Height)
         // Optionally, you can check if the result of VirtualFree is not zero.
         // Print out an error message if it is.
     }
-    
+
     Buffer->Width = Width;
     Buffer->Height = Height;
     Buffer->BytesPerPixel = 4;
     Buffer->Pitch = Buffer->Width * Buffer->BytesPerPixel;
-    
+
     BITMAPINFO Info = {};
     Buffer->Info.bmiHeader.biSize = sizeof(Buffer->Info.bmiHeader);
     Buffer->Info.bmiHeader.biWidth = Buffer->Width;
@@ -76,76 +95,69 @@ Win32ResizeDIBSection(win32_offscreen_buffer *Buffer, int Width, int Height)
     Buffer->Info.bmiHeader.biPlanes = 1;
     Buffer->Info.bmiHeader.biBitCount = 32;
     Buffer->Info.bmiHeader.biCompression = BI_RGB;
-    
+
     int BitmapMemorySize = Buffer->BytesPerPixel * (Buffer->Width * Buffer->Height);
-    Buffer->Memory = VirtualAlloc(0, BitmapMemorySize, MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);   
+    Buffer->Memory = VirtualAlloc(0, BitmapMemorySize, MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
 }
 
 internal void
-Win32DisplayBufferInWindow(win32_offscreen_buffer *Buffer, HDC DeviceContext, RECT ClientRect)
+Win32DisplayBufferInWindow(win32_offscreen_buffer *Buffer, HDC DeviceContext, int WindowWidth, int WindowHeight)
 {
-    int WindowWidth = ClientRect.right - ClientRect.left;
-    int WindowHeight = ClientRect.bottom - ClientRect.top;
-    
-    StretchDIBits(DeviceContext, 
+    StretchDIBits(DeviceContext,
                   0, 0, WindowWidth, WindowHeight, // destination rectangle (window)
                   0, 0, Buffer->Width, Buffer->Height, // source rectangle (bitmap buffer)
                   Buffer->Memory,
                   &Buffer->Info,
-                  DIB_RGB_COLORS, SRCCOPY);  
+                  DIB_RGB_COLORS, SRCCOPY);
 }
 
 LRESULT CALLBACK
-Win32MainWindowCallback(HWND Window, 
+Win32MainWindowCallback(HWND Window,
                         UINT Message,
                         WPARAM WParam,
                         LPARAM LParam)
 {
     LRESULT Result = 0;
-    
+
     switch (Message)
     {
         case WM_SIZE:
         {
-            RECT ClientRect;
-            GetClientRect(Window, &ClientRect);
-            int Width = ClientRect.right - ClientRect.left;
-            int Height = ClientRect.bottom - ClientRect.top;
-            Win32ResizeDIBSection(&GlobalBackbuffer, Width, Height);
+            win32_window_dimension Dimension = Win32GetWindowDimension(Window);
+            Win32ResizeDIBSection(&GlobalBackbuffer, Dimension.Width, Dimension.Height);
         } break;
-        
+
         case WM_DESTROY:
         {
             Running = false;
         } break;
-        
+
         case WM_CLOSE:
         {
             Running = false;
         } break;
-        
+
         case WM_ACTIVATEAPP:
         {
             OutputDebugStringA("WM_ACTIVATEAPP\n");
         } break;
-        
+
         case WM_PAINT:
         {
             PAINTSTRUCT Paint;
-
             HDC DeviceContext = BeginPaint(Window, &Paint);
-            RECT ClientRect;
-            GetClientRect(Window, &ClientRect);
-            Win32DisplayBufferInWindow(&GlobalBackbuffer, DeviceContext, ClientRect);
+
+            win32_window_dimension Dimension = Win32GetWindowDimension(Window);
+            Win32DisplayBufferInWindow(&GlobalBackbuffer, DeviceContext, Dimension.Width, Dimension.Height);
             EndPaint(Window, &Paint);
         } break;
-        
+
         default:
         {
             Result = DefWindowProc(Window, Message, WParam, LParam);
         } break;
     }
-    
+
     return (Result);
 }
 
@@ -160,9 +172,9 @@ WinMain(HINSTANCE Instance,
     WindowClass.lpfnWndProc = Win32MainWindowCallback;
     WindowClass.hInstance = Instance;
     // WindowClass.hCursor;
-    // WindowClass.hIcon; 
+    // WindowClass.hIcon;
     WindowClass.lpszClassName = "Win32WindowClass";
-    
+
     if(RegisterClassA(&WindowClass))
     {
         HWND Window =
@@ -179,14 +191,14 @@ WinMain(HINSTANCE Instance,
                             0,
                             Instance,
                             0);
-        if(Window)
+        if (Window)
         {
             // Since we specified CS_OWNDC, we can just
             // get one device context and use it forever because we
             // are not sharing it with anyone.
+            HDC DeviceContext = GetDC(Window);
             int XOffset = 0;
             int YOffset = 0;
-            HDC DeviceContext = GetDC(Window);
 
             Running = true;
             while(Running)
@@ -198,18 +210,16 @@ WinMain(HINSTANCE Instance,
                     {
                         Running = false;
                     }
-                    
+
                     TranslateMessage(&Message);
                     DispatchMessageA(&Message);
                 }
-                
+
                 RenderGradient(&GlobalBackbuffer, XOffset, YOffset);
                 ++XOffset;
-
-                RECT ClientRect;
-                GetClientRect(Window, &ClientRect);
-
-                Win32DisplayBufferInWindow(&GlobalBackbuffer, DeviceContext, ClientRect);
+                
+                win32_window_dimension Dimension = Win32GetWindowDimension(Window);
+                Win32DisplayBufferInWindow(&GlobalBackbuffer, DeviceContext, Dimension.Width, Dimension.Height);
             }
         }
         else
@@ -221,6 +231,6 @@ WinMain(HINSTANCE Instance,
     {
         // TODO: Logging
     }
-    
+
     return(0);
 }
